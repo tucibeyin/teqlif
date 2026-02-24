09a7d438e0eb7e45ea0ce3d0c36f70ef19efe2e5
tucibeyin <tucibeyin@hotmail.com>
2026-02-24 20:30:58 +0300

    Push Notification

--- mobile/ios/Runner/AppDelegate.swift
+++ mobile/ios/Runner/AppDelegate.swift
@@ -9,10 +9,28 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
+    
+    // EKLENEN KISIM 1: Apple'dan cihazı bildirimlere kaydetmesini istiyoruz
+    application.registerForRemoteNotifications()
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+    
+  // EKLENEN KISIM 2: Apple Token VERİRSE bu fonksiyon çalışır
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+      let token = tokenParts.joined()
+      print("🍏 [NATIVE APNS] Apple Token Başarıyla Alındı: \(token)")
+      super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+
+  // EKLENEN KISIM 3: Apple Token VERMEYİ REDDEDERSE bu fonksiyon çalışır ve sebebi söyler
+  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+      print("🍎 [NATIVE APNS HATA] Apple Token VERMEDİ! Gerçek Neden: \(error.localizedDescription)")
+      super.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
   }
 }
