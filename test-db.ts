fc39b495e6db0d55b8827d637a04bc6e63ed9a70
tucibeyin <tucibeyin@hotmail.com>
2026-02-24 16:42:10 +0300

    Add iOS Push Notification and Background Modes capabilities to Xcode project

--- test-db.ts
+++ test-db.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-async function main() {
-  const p = await prisma.province.findFirst()
-  console.log("Province:", p)
-  const d = await prisma.district.findFirst()
-  console.log("District:", d)
+async function run() {
+  const users = await prisma.user.findMany({
+    select: { id: true, email: true, name: true, fcmToken: true }
+  });
+  console.log(users);
 }
-main()
+run();
