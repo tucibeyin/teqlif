9e0a5c1ff4618ec8c42e4f4ab2cb929cdef3dd06
tucibeyin <tucibeyin@hotmail.com>
2026-07-05 14:46:41 +0300

    fix(mobile): update import paths in faq_screen to resolve flutter analyze errors

--- mobile/lib/screens/faq_screen.dart
+++ mobile/lib/screens/faq_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import '../core/theme/app_colors.dart';
+import '../l10n/app_localizations.dart';
+import '../config/app_colors.dart';
 
 class FaqScreen extends StatelessWidget {
   const FaqScreen({super.key});
