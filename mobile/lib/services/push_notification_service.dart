15b0d67c3c20a940b3d25d61856beea62188a732
tucibeyin <tucibeyin@hotmail.com>
2026-07-13 01:15:20 +0300

    fix(mobile): add missing dart:io import for Platform check

--- mobile/lib/services/push_notification_service.dart
+++ mobile/lib/services/push_notification_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
